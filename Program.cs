5660dd600035d7ff5f3e5a8ea9ed3a0a253270bd
ZernZ <104818587+ZernZz@users.noreply.github.com>
2023-05-19 23:23:47 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+class Program
+{
+    static bool IsValidSequence(string halfDNASequence) => halfDNASequence.All("ATCG".Contains);
+    static string ReplicateSequence(string halfDNASequence) => new string(halfDNASequence.Select(n => "TAGC"["ATCG".IndexOf(n)]).ToArray());
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("Enter a half DNA sequence: ");
+            string halfDNASequence = Console.ReadLine();
+
+            if (IsValidSequence(halfDNASequence))
+            {
+                Console.WriteLine("Current half DNA sequence: " + halfDNASequence);
+
+                bool validReplicateOption = false;
+                while (!validReplicateOption)
+                {
+                    Console.Write("Do you want to replicate it? (Y/N): ");
+                    string replicateOption = Console.ReadLine().ToUpper();
+
+                    switch (replicateOption)
+                    {
+                        case "Y":
+                            string replicatedSequence = ReplicateSequence(halfDNASequence);
+                            Console.WriteLine("Replicated half DNA sequence: " + replicatedSequence);
+                            validReplicateOption = true;
+                            break;
+                        case "N":
+                            validReplicateOption = true;
+                            break;
+                        default:
+                            Console.WriteLine("Please input Y or N.");
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("That half DNA sequence is invalid.");
+            }
+            Console.Write("Do you want to process another sequence? (Y/N): ");
+            string continueOption = Console.ReadLine().ToUpper();
+            if (continueOption == "N")
+                break;
+        }
+    }
+}
